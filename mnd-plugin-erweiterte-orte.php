8440525b804733fa497eb5c835cf2849e456fee8
mnd-ander <45659924+mnd-ander@users.noreply.github.com>
2019-03-05 12:15:25 +0100

    versionsnummer++ nach all den änderungen

--- mnd-plugin-erweiterte-orte.php
+++ mnd-plugin-erweiterte-orte.php
@@ -5,10 +5,10 @@ Plugin URI: https://www.shakuras.me
 Description: Bitte die Bedienungsanleitung beachten (unter Events->Anderbox). Dieses Plugin erweitert die Funktionalität des Events Manager Plugins um Orttypen. 
 Author: Alexander Schmidt
 Author URI: https://www.shakuras.me
-Version: 0.8.14
+Version: 0.9.1
 */
 
-define('MND_VERSION', 0.0814); 
+define('MND_VERSION', 0.0901); 
 /*
 versionsnummern sind a.b.c
 a = erfüllt alle aktuellen anforderungen
